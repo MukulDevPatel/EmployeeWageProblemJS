b7ddc84e73b92fb41fac9d93de4e0a35677d7e5e
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-17 09:11:23 +0530

    [Mukul Dev] Add: Calculate total emp wage for a month

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,9 +1,10 @@
-//UC3
+//UC4
 const Part_Time = 1;
 const Full_Time = 2;
 const Part_Time_Hrs = 4;
 const Full_Time_Hrs = 8;
 const Wage_Per_Hrs = 20;
+const Num_Of_Working_Days = 20;
 let empHrs = 0;
 let Daily_Wage;
 var empCheck =  Math.floor(Math.random() * 10) % 3;
@@ -22,5 +23,10 @@ function getWorkingHours(empCheck) {
   }
 }
 empHrs = getWorkingHours(empCheck);
-Daily_Wage = empHrs * Wage_Per_Hrs;
-console.log("Employee wage is: " + Daily_Wage);
+let totalEmpHrs = 0;
+for (let day = 0; day <= Num_Of_Working_Days; day++){
+    let empCheck = Math.floor(Math.random() * 10)%3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+Daily_Wage = totalEmpHrs * Wage_Per_Hrs;
+console.log("Employee wage is: " + Daily_Wage + ",\nHours: " + totalEmpHrs);
