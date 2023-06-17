aac592f36fb5f002cf3627fd18cfab2cf705a049
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-17 08:42:22 +0530

    [Mukul Dev] Add: Calculate daily wage of employee based on part time and full time

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,11 +1,22 @@
-//UC1
-const IS_Absent =1;
-let empCheck = Math.floor(Math.random() * 10) %2;
-if (empCheck == IS_Absent)
-{
-    console.log("Employee is absent"); return;
-}
-else 
-{
-    console.log("Employee is present");
-}
+//UC2
+const Part_Time = 1;
+const Full_Time = 2;
+const Part_Time_Hrs = 4;
+const Full_Time_Hrs = 8;
+const Wage_Per_Hrs = 20;
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+ switch (empCheck)
+ {
+    case Part_Time:
+        empHrs = Part_Time_Hrs;
+        break;
+    case Full_Time:
+        empHrs = Full_Time_Hrs;
+        break;
+    default:
+        empHrs = 0;
+        break;
+  }
+let Daily_Wage = empHrs * Wage_Per_Hrs;
+console.log("Employee wage is: " + Daily_Wage);
