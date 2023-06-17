ba189553db9f3b9fafe061207044f7072ea557af
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-17 09:35:58 +0530

    [Mukul Dev] Add: Calculate total emp wage for a month and total working hrs in a month

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,13 +1,16 @@
-//UC4
+//UC5
 const Part_Time = 1;
 const Full_Time = 2;
 const Part_Time_Hrs = 4;
 const Full_Time_Hrs = 8;
 const Wage_Per_Hrs = 20;
+
 const Num_Of_Working_Days = 20;
-let empHrs = 0;
-let Daily_Wage;
-var empCheck =  Math.floor(Math.random() * 10) % 3;
+const Max_Hrs_In_Month = 160;
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;;
+
 function getWorkingHours(empCheck) {
  switch (empCheck)
  {
@@ -22,11 +25,12 @@ function getWorkingHours(empCheck) {
         break;
   }
 }
-empHrs = getWorkingHours(empCheck);
-let totalEmpHrs = 0;
-for (let day = 0; day <= Num_Of_Working_Days; day++){
+
+
+while (totalEmpHrs<=Max_Hrs_In_Month && totalWorkingDays<Num_Of_Working_Days){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10)%3;
     totalEmpHrs += getWorkingHours(empCheck);
 }
-Daily_Wage = totalEmpHrs * Wage_Per_Hrs;
-console.log("Employee wage is: " + Daily_Wage + ",\nHours: " + totalEmpHrs);
+let empWage = totalEmpHrs * Wage_Per_Hrs;
+console.log("UC-Total Days: " + totalWorkingDays + ",\nTotal Hours: " + totalEmpHrs + "\nEmployee wage is: " + empWage);
