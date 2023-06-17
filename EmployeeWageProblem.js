13b9dff873659d007f107301a74f7266a78476b1
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-17 08:49:26 +0530

    [Mukul Dev] Add: Refactor the code and use get work function for calculating daily wage

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,22 +1,26 @@
-//UC2
+//UC3
 const Part_Time = 1;
 const Full_Time = 2;
 const Part_Time_Hrs = 4;
 const Full_Time_Hrs = 8;
 const Wage_Per_Hrs = 20;
 let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
+let Daily_Wage;
+var empCheck =  Math.floor(Math.random() * 10) % 3;
+function getWorkingHours(empCheck) {
  switch (empCheck)
  {
     case Part_Time:
-        empHrs = Part_Time_Hrs;
+        return Part_Time_Hrs;
         break;
     case Full_Time:
-        empHrs = Full_Time_Hrs;
+        return Full_Time_Hrs;
         break;
     default:
-        empHrs = 0;
+        return 0;
         break;
   }
-let Daily_Wage = empHrs * Wage_Per_Hrs;
+}
+empHrs = getWorkingHours(empCheck);
+Daily_Wage = empHrs * Wage_Per_Hrs;
 console.log("Employee wage is: " + Daily_Wage);
