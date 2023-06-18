b50e22a13ea693c20524adbde444964f36852017
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-18 20:40:56 +0530

    [Mukul Dev] Add: In payroll data, name should be start with capital and use regex method

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,4 +1,4 @@
-//UC11
+//UC13
 class EmployeePayrollData{
     //property
     id;
@@ -19,24 +19,37 @@ class EmployeePayrollData{
     //getter and setter method
     get name() {return this._name;}
     set name(name) {
-        console.log("Setting: " + name);
-        this._name = name;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name)) {
+            console.log("Setting: " + name);
+            this._name = name;
+        }
+        else throw "Name is Incorrect"; 
     }
 
     //method
     toString() {
         const options = {year: 'numeric', month: 'long', day: 'numeric'};
-        const empDate = this.startDate == undefined ? "undefined" : 
+        const empDate = !this.startDate ? "undefined" : 
         this.startDate.toLocaleDateString("en-US", options);
         return "Id = " + this.id + ", Name = "+ this.name + ", Salary = "+ this.salary + ", Gender: " + 
         this.gender + ", Start date: " + empDate;
     }
 }
 
-let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000, 'M');
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000, 'M', new Date());
 console.log(employeePayrollData.toString());
-employeePayrollData.name = "John";
-console.log(employeePayrollData.toString());
+
+try{
+    employeePayrollData.name = "John";
+    console.log(employeePayrollData.toString());
+    employeePayrollData.name = "edward";
+    console.log(employeePayrollData.toString());
+    employeePayrollData.name = "Jo";
+    console.log(employeePayrollData.toString());
+}catch(e){
+    console.error(e);
+}
 
 let newEmployeePayrollData = new EmployeePayrollData(2, "Terrisa", 20000, 'F', new Date());
 console.log(newEmployeePayrollData.toString());
