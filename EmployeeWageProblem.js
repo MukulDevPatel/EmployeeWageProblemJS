702a70147d7669cecd14700ab218bb4a615ad0d8
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-18 14:08:43 +0530

    [Mukul Dev] Add: Store the day hours worked and wage earned in a single object

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,4 +1,4 @@
-//UC9
+//UC10
 const Part_Time = 1;
 const Full_Time = 2;
 const Part_Time_Hrs = 4;
@@ -26,9 +26,7 @@ function getWorkingHours(empCheck) {
 
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
-let empDailyWageArr = new Array();
-let empDailyHrsMap = new Map();
-let empDailyWageMap = new Map();
+let empDailyHrsAndWageArr = new Array();
 
 function calDailyWage(totalEmpHrs){
     return totalEmpHrs * Wage_Per_Hrs;
@@ -39,31 +37,14 @@ while (totalEmpHrs<=Max_Hrs_In_Month && totalWorkingDays<Num_Of_Working_Days){
     let empCheck = Math.floor(Math.random() * 10)%3;
     let empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
-    empDailyWageArr.push(calDailyWage(totalEmpHrs));
-    empDailyHrsMap.set(totalWorkingDays,empHrs);
-    empDailyWageMap.set(totalWorkingDays,calDailyWage(empHrs));
+    empDailyHrsAndWageArr.push( {
+        dayNum:totalWorkingDays,
+        dailyHours:empHrs,
+        dailyWage: calDailyWage(empHrs),
+        toString() {
+            return "\nDay "+this.dayNum + " => Working Hours is " + this.dailyHours + " And wage earned = " + this.dailyWage
+        },
+    });
 }
 
-const findTotal = (TotalVal, DailyVal)=>TotalVal + DailyVal;
-
-let count = 0;
-let TotalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
-let TotalSalary = empDailyWageArr.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
-console.log("UC9A-Emp Wage with Arrow: " + " Total Hours: "+ TotalHours+ " Total Wages: "+ TotalSalary);
-
-let NonWorkingDays = new Array();
-let PartWorkingDays = new Array();
-let FullWorkingDays = new Array();
-
-empDailyHrsMap.forEach( (value,key)=> {
-    if (value == 8) FullWorkingDays.push(key);
-    else if (value == 4) PartWorkingDays.push(key);
-    else NonWorkingDays.push(key);
-});
-
-console.log("=====Full Working Days=====");
-console.log(FullWorkingDays);
-console.log("=====Part Working Days=====");
-console.log( PartWorkingDays);
-console.log("=====Non Working Days=====");
-console.log(NonWorkingDays);
+console.log("UC10-Showing daily worked hours and wage: " + empDailyHrsAndWageArr);
