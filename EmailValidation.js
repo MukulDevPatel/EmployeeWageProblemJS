b73905645c801cca1b2cb8cde46182a5be13e863
MukulDevPatel <patelmukuldev@gmail.com>
2023-07-07 20:27:55 +0530

    [Mukul Dev] Add: We create email id validation js file and use new regex pattern email will be valid

--- EmailValidation.js
+++ EmailValidation.js
@@ -3,3 +3,9 @@ const emailPattern = /^abc(\.[a-zA-Z]+)?@bridgelabz\.co\.in$/;
 const email = "abc.xyz@bridgelabz.co.in";
 const isValid = emailPattern.test(email);
 console.log("Is Valid:", isValid);
+
+//UC2
+const emailPattern1 = /^abc\.[a-zA-Z]+@bridgelabz(\.[a-zA-Z]+)?\.co\.in$/;
+const email1 = "abc.xyz@bridgelabz.co.in";
+const isValid1 = emailPattern.test(email1);
+console.log("Is Valid:", isValid1);
