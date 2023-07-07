a911359725600ba170803d76ea42ceeba32c8438
MukulDevPatel <125425636+MukulDevPatel@users.noreply.github.com>
2023-07-07 19:07:45 +0530

    Update PincodeValidation.js

--- PincodeValidation.js
+++ PincodeValidation.js
@@ -12,7 +12,7 @@ const pinCode2 = "A400088";
 const isValid = pinCodePattern.test(pinCode2);
 console.log("Is Valid: ",isValid);
 
-// UC2
+// UC3
 const pinCode3 = "400088B";
 const isValidate = pinCodePattern.test(pinCode3);
 console.log("Is Valid: ",isValidate);
